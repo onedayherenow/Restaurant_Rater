214a288559bb06e59b5e30bb143ec18d8250fe09
onedayherenow@gmail.com <onedayherenow@gmail.com>
2021-10-22 19:15:09 -0400

    cshtml

---
+++ Restaurant_Rater/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant_Rater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

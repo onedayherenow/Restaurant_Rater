7709cd7df2865156f6ad52103f8537ef567a492c
onedayherenow@gmail.com <onedayherenow@gmail.com>
2021-10-25 15:13:02 -0400

    Create View

--- Restaurant_Rater/Controllers/RestaurantController.cs
+++ Restaurant_Rater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurant_Rater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace Restaurant_Rater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
         // GET: Restaurant
         public ActionResult Index()
         {
+            return View(_db.Restaurants.ToList());
+        }
+
+        // GET: Restaurant/Create
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }

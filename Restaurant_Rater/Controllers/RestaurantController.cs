20c17ba922d42ffc57eeb6e20cc3baee5345f3f1
onedayherenow@gmail.com <onedayherenow@gmail.com>
2021-10-26 16:09:01 -0400

    Delete Functionality Added

--- Restaurant_Rater/Controllers/RestaurantController.cs
+++ Restaurant_Rater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -37,6 +38,32 @@ namespace Restaurant_Rater.Controllers
             }
 
             return View(restaurant);
+        }
+
+        // GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)   //makes the int nullable
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);   //if id is null, bad request result
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);  //finds the restaurant entity in the database by id
+            if (restaurant == null)  
+            {
+                return HttpNotFound();  //if restaurant doesn't exist it returns not found result
+            }
+            return View(restaurant);  //if restaurant exists return the view
+        }
+
+        // POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
